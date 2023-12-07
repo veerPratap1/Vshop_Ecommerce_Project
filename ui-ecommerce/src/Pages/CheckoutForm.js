987dad5bb1216239a05f26acdfb2e59d5608d2c4
Vijay Pratap <vijayrana81203@gmail.com>
2023-12-07 11:52:52 +0530

    "updated"

--- ui-ecommerce/src/Pages/CheckoutForm.js
+++ ui-ecommerce/src/Pages/CheckoutForm.js
@@ -62,7 +62,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `http://localhost:8000/order-success/${currentOrder?.id}`,
+        return_url: `https://vshop-ecommerce-project-git-master-vijay-prataps-projects.vercel.app/order-success/${currentOrder?.id}`,
       },
     });
 
